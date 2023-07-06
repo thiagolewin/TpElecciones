e0cb29b2d99ad1dcb3cc49ddcfb72694dec62c95
THIAGO NICOLÁS LEWIN <47393575@ort>
2023-07-06 15:54:17 -0300

    Subida dia 6/7

--- Models/Candidato.cs
+++ Models/Candidato.cs
@@ -6,6 +6,6 @@ public class Candidato {
     public DateTime FechaNacimiento { set;  get;}
     public string foto { set;  get;}
     public string postulacion { set;  get;}
-
+    public string WikiPedia {set; get;}
 
 }
